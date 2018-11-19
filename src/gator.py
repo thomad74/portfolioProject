3d03cfbe235f478a6c35479383a7edfda9625c58
Dillon Thoma <thomad@aldenv187.allegheny.edu>
2018-11-19 11:27:21 -0500

    checkstyle edits

--- src/gator.py
+++ src/gator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""application.py  Python Program that creates a pop-up window with a gator swimming through a body of water. """
+"""application.py  Python Program that creates a pop-up window with
+a gator swimming through a body of water. """
 
-__author__      = "Dillon Thoma"
-__date__        = "15 November 2018"
+__author__ = "Dillon Thoma"
+__date__ = "15 November 2018"
 
 # MUST import the pygame package to run this program.
 import pygame
@@ -21,17 +22,18 @@ segment_margin = 4
 x_change = segment_width + segment_margin
 y_change = 0
 
+
 class Segment(pygame.sprite.Sprite):
     # Constructor
     def __init__(self, x, y):
         # Call the parent's constructor
         super().__init__()
 
-        # Set height & width of the gator segments, and choose the color as GREEN.
+# Set height & width of the gator segments, and choose the color as GREEN.
         self.image = pygame.Surface([segment_width, segment_height])
         self.image.fill(GREEN)
 
-        # Creates the gator's starting point as the top left corner of the screen.
+# Creates the gator's starting point as the top left corner of the screen.
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
